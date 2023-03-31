a5f366b3a18092a9ccd6736d91d4d4d8e1c87012
DarkJoker360 <simoespo159@gmail.com>
2023-03-31 16:18:41 +0000

    HACK: telephony: Conditionally force enable LTE_CA
    * Apparently since S LTE_CA is not properly working,
      let's conditionally force enable it until a proper
      solution is found.
    
    Change-Id: Ief39159d0b34d53b0c45e21b732cbc30a1713eff
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- telephony/java/android/telephony/NetworkRegistrationInfo.java
+++ telephony/java/android/telephony/NetworkRegistrationInfo.java
@@ -23,6 +23,7 @@ import android.annotation.SystemApi;
 import android.annotation.TestApi;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.SystemProperties;
 import android.telephony.AccessNetworkConstants.TransportType;
 import android.telephony.Annotation.NetworkType;
 import android.text.TextUtils;
@@ -481,6 +482,13 @@ public final class NetworkRegistrationInfo implements Parcelable {
      * @hide
      */
     public void setAccessNetworkTechnology(@NetworkType int tech) {
+        // HACK: Force LTE Carrier Aggregation
+        if (SystemProperties.getBoolean("persist.sys.radio.force_lte_ca", false)
+                && tech == TelephonyManager.NETWORK_TYPE_LTE) {
+            if (mDataSpecificInfo != null) {
+                mDataSpecificInfo.setIsUsingCarrierAggregation(true);
+            }
+        }
         if (tech == TelephonyManager.NETWORK_TYPE_LTE_CA) {
             // For old device backward compatibility support
             tech = TelephonyManager.NETWORK_TYPE_LTE;
